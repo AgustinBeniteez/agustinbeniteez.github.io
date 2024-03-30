17065ede917c062e18af51d7cccb71c75ff28577
AgusTheKing <adivinanzas1991@gmail.com>
2024-03-30 14:12:34 +0100

    Update Aboutme

--- tutorials/menujs.js
+++ tutorials/menujs.js
@@ -33,4 +33,16 @@
             button.style.display = "none";
         }
 
-        
+             // NAV menu movil
+   document.addEventListener('DOMContentLoaded', function () {
+    // Obtener referencias al botón de menú y al menú
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.querySelector('nav ul');
+    
+    // Agregar un event listener para el clic en el botón de menú
+    menuToggle.addEventListener('click', function() {
+        // Alternar la clase 'active' en el menú para mostrarlo u ocultarlo
+        navMenu.classList.toggle('active');
+    });
+    
+    });
