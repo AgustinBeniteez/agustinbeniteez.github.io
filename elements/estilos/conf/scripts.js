ad29d79b1a8447e177b6099cfa5ac6b736861c9d
AgustinBeniteez <adivinanzas1991@gmail.com>
2024-06-28 14:44:56 +0200

    Update 1.0.14
    boton de load more elements and fix dark mode and light mode post

--- elements/estilos/conf/scripts.js
+++ elements/estilos/conf/scripts.js
@@ -1,4 +1,3 @@
-// When the document is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
     // Show spinner when posts are being loaded
     document.getElementById('spinner-container').style.display = 'block';
@@ -46,6 +45,14 @@ document.addEventListener('DOMContentLoaded', function() {
         currentPostIndex += postsPerPage;
         initializeCheckBoxes(); // Initialize the checkbox event listener after posts are loaded
         document.getElementById('spinner-container').style.display = 'none'; // Hide spinner after posts are loaded
+
+        // Check if there are more posts to load
+        if (currentPostIndex >= postsData.length) {
+            // No more posts to load
+            const loadMoreBtn = document.getElementById('load-more-btn');
+            loadMoreBtn.textContent = 'No more elements';
+            loadMoreBtn.disabled = true;
+        }
     }
 
     // Function to initialize checkbox event listener
@@ -80,85 +87,81 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Add scroll event listener to load more posts when scrolling down
-    window.addEventListener('scroll', function() {
-        const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-        if (scrollTop + clientHeight >= scrollHeight - 5) {
-            if (currentPostIndex < postsData.length) {
-                loadMorePosts();
-            } else {
-                // Show hidden posts if all posts have been loaded
-                const hiddenPosts = document.querySelectorAll('.post[style*="display: none"]');
-                hiddenPosts.forEach(post => {
-                    post.style.display = 'block';
-                });
-            }
+    // Load More button event listener
+    document.getElementById('load-more-btn').addEventListener('click', function() {
+        if (currentPostIndex < postsData.length) {
+            loadMorePosts();
+        } else {
+            // No more posts to load
+            const loadMoreBtn = document.getElementById('load-more-btn');
+            loadMoreBtn.textContent = 'No more elements';
+            loadMoreBtn.disabled = true;
         }
     });
-});
 
-   
-$(document).ready(function() {
-    var $searchInput = $('#search-box');
-    var $tagButtons = $('.tag-button');
-    var $posts = $('.post');
-    var $noResultsMessage = $('#no-results-message');
+    $(document).ready(function() {
+        var $searchInput = $('#search-box');
+        var $tagButtons = $('.tag-button');
+        var $posts = $('.post');
+        var $noResultsMessage = $('#no-results-message');
 
-    $searchInput.on('keyup', function() {
-        var searchTerm = $(this).val().toLowerCase();
-        var matchingPosts = $posts.filter(function() {
-            var postTitle = $(this).find('.post-title').text().toLowerCase();
-            return postTitle.includes(searchTerm);
-        });
-    
-        if (matchingPosts.length > 0) {
-            $noResultsMessage.hide();
-            $posts.hide();
-            matchingPosts.show();
-        } else {
-            $posts.hide();
-            // Ocultar el mensaje solo si hay al menos 1 post visible
-            if ($('.post:visible').length > 0) {
+        $searchInput.on('keyup', function() {
+            var searchTerm = $(this).val().toLowerCase();
+            var matchingPosts = $posts.filter(function() {
+                var postTitle = $(this).find('.post-title').text().toLowerCase();
+                return postTitle.includes(searchTerm);
+            });
+
+            if (matchingPosts.length > 0) {
                 $noResultsMessage.hide();
+                $posts.hide();
+                matchingPosts.show();
             } else {
+                $posts.hide();
+                // Ocultar el mensaje solo si hay al menos 1 post visible
+                if ($('.post:visible').length > 0) {
+                    $noResultsMessage.hide();
+                } else {
+                    $noResultsMessage.show();
+                }
+            }
+            // Disable scroll event while searching
+            window.removeEventListener('scroll', onScrollLoadMore);
+        });
+
+        $tagButtons.on('click', function() {
+            var tag = $(this).data('tag').toLowerCase();
+            var matchingPosts = $posts.filter(function() {
+                var postTags = $(this).data('tags') || '';
+                return postTags.split(',').map(function(t) {
+                    return t.trim();
+                }).includes(tag);
+            });
+
+            if (matchingPosts.length > 0) {
+                $noResultsMessage.hide();
+                $posts.hide();
+                matchingPosts.show();
+            } else {
+                $posts.hide();
                 $noResultsMessage.show();
             }
-        }
-    });
-    
-    
-
-    $tagButtons.on('click', function() {
-        var tag = $(this).data('tag').toLowerCase();
-        var matchingPosts = $posts.filter(function() {
-            var postTags = $(this).data('tags') || '';
-            return postTags.split(',').map(function(t) {
-                return t.trim();
-            }).includes(tag);
+            // Disable scroll event while filtering by tags
+            window.removeEventListener('scroll', onScrollLoadMore);
         });
 
-        if (matchingPosts.length > 0) {
-            $noResultsMessage.hide();
-            $posts.hide();
-            matchingPosts.show();
-        } else {
-            $posts.hide();
-            $noResultsMessage.show();
-        }
-    });
-
-    // Agrega esta función para mostrar el mensaje cuando se elimine el texto de búsqueda y se muestren todos los posts nuevamente
-    $searchInput.on('input', function() {
-        var searchTerm = $(this).val().toLowerCase();
-        if (searchTerm === '') {
-            $noResultsMessage.hide();
-        }
+        // Agrega esta función para mostrar el mensaje cuando se elimine el texto de búsqueda y se muestren todos los posts nuevamente
+        $searchInput.on('input', function() {
+            var searchTerm = $(this).val().toLowerCase();
+            if (searchTerm === '') {
+                $noResultsMessage.hide();
+                // Enable scroll event when search is cleared
+                window.addEventListener('scroll', onScrollLoadMore);
+            }
+        });
     });
 });
 
-
-   
-   
 // BORRADOR
 function limpiarBusqueda() {
     // Limpiar el contenido del input de búsqueda
@@ -172,77 +175,75 @@ function limpiarBusqueda() {
 
     // Recargar los posts en la página actual
     loadPosts(currentPage);
+
+    // Enable scroll event when clearing search
+    window.addEventListener('scroll', onScrollLoadMore);
 }
 
+// Variable to store the current page number
+let currentPage = 1;
 
-   
-   // Variable to store the current page number
-   let currentPage = 1;
-   
-   document.addEventListener('DOMContentLoaded', function() {
-   // Function to load posts for the given page
-   function loadPosts(page) {
-       // Hide all posts
-       $('.post').hide();
-       // Calculate the starting index of the posts for the current page
-       const startIndex = (page - 1) * 8;
-       // Calculate the ending index of the posts for the current page
-       const endIndex = startIndex + 8;
-       // Show only the posts corresponding to the current page within the range of startIndex and endIndex
-       $('.post').slice(startIndex, endIndex).show();
-       // Display the current page number
-       $('#current-page').text(page);
-   }
-   
-   
-       // Call the function to load posts when the page loads
-       loadPosts(currentPage);
-   
-       // Event handler for the previous page button
-       $('#prev-page').on('click', function() {
-           if (currentPage > 1) {
-               currentPage--;
-               loadPosts(currentPage);
-           }
-       });
-   
-       // Event handler for the next page button
-       $('#next-page').on('click', function() {
-           // Calculate the total number of pages
-           const totalPages = Math.ceil($('.post:visible').length / 8);
-           if (currentPage < totalPages) {
-               currentPage++;
-               loadPosts(currentPage);
-           }
-       });
-   
-       // Search functionality
-       $('#search-box').on('keyup', function() {
-           const searchTerm = $(this).val().toLowerCase().trim();
-           if (searchTerm === '') {
-               $('.post').show();
-           } else {
-               $('.post').hide().filter(function() {
-                   const postTitle = $(this).find('.post-title').text().toLowerCase();
-                   return postTitle.includes(searchTerm);
-               }).show();
-           }
-       });
-   });
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to load posts for the given page
+    function loadPosts(page) {
+        // Hide all posts
+        $('.post').hide();
+        // Calculate the starting index of the posts for the current page
+        const startIndex = (page - 1) * 8;
+        // Calculate the ending index of the posts for the current page
+        const endIndex = startIndex + 8;
+        // Show only the posts corresponding to the current page within the range of startIndex and endIndex
+        $('.post').slice(startIndex, endIndex).show();
+        // Display the current page number
+        $('#current-page').text(page);
+    }
 
+    // Call the function to load posts when the page loads
+    loadPosts(currentPage);
 
-     // NAV menu movil
-   document.addEventListener('DOMContentLoaded', function () {
-// Obtener referencias al botón de menú y al menú
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.querySelector('nav ul');
+    // Event handler for the previous page button
+    $('#prev-page').on('click', function() {
+        if (currentPage > 1) {
+            currentPage--;
+            loadPosts(currentPage);
+        }
+    });
 
-// Agregar un event listener para el clic en el botón de menú
-menuToggle.addEventListener('click', function() {
-    // Alternar la clase 'active' en el menú para mostrarlo u ocultarlo
-    navMenu.classList.toggle('active');
+    // Event handler for the next page button
+    $('#next-page').on('click', function() {
+        // Calculate the total number of pages
+        const totalPages = Math.ceil($('.post:visible').length / 8);
+        if (currentPage < totalPages) {
+            currentPage++;
+            loadPosts(currentPage);
+        }
+    });
+
+    // Search functionality
+    $('#search-box').on('keyup', function() {
+        const searchTerm = $(this).val().toLowerCase().trim();
+        if (searchTerm === '') {
+            $('.post').show();
+        } else {
+            $('.post').hide().filter(function() {
+                const postTitle = $(this).find('.post-title').text().toLowerCase();
+                return postTitle.includes(searchTerm);
+            }).show();
+        }
+    });
 });
 
+// NAV menu movil
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtener referencias al botón de menú y al menú
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.querySelector('nav ul');
+
+    // Agregar un event listener para el clic en el botón de menú
+    menuToggle.addEventListener('click', function() {
+        // Alternar la clase 'active' en el menú para mostrarlo u ocultarlo
+        navMenu.classList.toggle('active');
+    });
 });
 
 // Cuando se carga el documento
@@ -288,6 +289,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
 document.addEventListener("DOMContentLoaded", function() {
     const tagButtons = document.querySelectorAll('.tag-button');
     const posts = document.querySelectorAll('.post');
@@ -314,10 +316,11 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 noResultsMessage.style.display = 'block';
             }
+            // Disable scroll event while filtering by tags
+            window.removeEventListener('scroll', onScrollLoadMore);
         });
     });
 });
-
 
 document.addEventListener("DOMContentLoaded", function() {
     const tagButtons = document.querySelectorAll('.tag-button');
@@ -343,4 +346,3 @@ document.addEventListener('DOMContentLoaded', function() {
         Promise.reject("Simulated network error");
     }, 3000); // Simulate a delay of 3 seconds
 });
-
